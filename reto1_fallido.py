b576251c30ab72d0e085074e73cae856f64c41db
dolchKT4 <120228067+notKT4@users.noreply.github.com>
2023-05-03 15:07:15 +0100

    Reto fallido 1

---
+++ reto1_fallido.py
@@ -0,0 +1,7 @@
+nombre = input ("Cual es tu nombre?")
+edad = input("Cual es tu edad?")
+total_edad = int(edad) + 20
+print(type(edad))
+
+formato = (f"Hola mi nombre es {nombre} , mi edad es {edad} , en 20 años tendre {str(total_edad)} años")
+print(formato)
