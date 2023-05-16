50092d1f296904c414e6d65ca1bc20e92b0d930b
dolchKT4 <120228067+notKT4@users.noreply.github.com>
2023-05-17 00:32:24 +0100

    17/05/23

--- Unidad-2-python/Guias/Guia-1/ejercicio4.py
+++ Unidad-2-python/Guias/Guia-1/ejercicio4.py
@@ -3,6 +3,20 @@ Elaborar un algoritmo que solicite al usuario su nombre y el nombre de otra
 persona. Luego, mostrar en pantalla un mensaje que indique si ambos nombres
 comienzan con la misma letra o si terminan con la misma letra, o si difieren tanto
 en la letra inicial como en la final. Por ejemplo, si los nombres ingresados son
-Belinda y Beatriz, se mostrara un mensaje que indique que ambos nombres comienzan con la misma letra. Si los nombres son Leonardo y Gonzalo, se mostrar´a
+Belinda y Beatriz, se mostrara un mensaje que indique que ambos nombres comienzan con la misma letra. Si los nombres son Leonardo y Gonzalo, se mostrara
 un mensaje que indique que ambos nombres terminan con la misma letra.
 '''
+nombre=str(input("Introduzca un nombre \n"))
+nombre1=str(input("Introduzca un segundo nombre \n"))
+if nombre[-1]==nombre1[-1] and nombre[0]==nombre1[0]:
+    print("Ambos nombres terminan y empiezan con la misma letra")
+elif nombre[-1]!=nombre1[-1] and nombre[0]!=nombre1[0]:
+    print("Ambos nombres difieren tanto en la primera letra como la ultima")
+elif nombre[0] == nombre1[0]:
+    print("Ambos nombres comienzan con la misma letra.")
+elif nombre[0]!=nombre1[0]: 
+    print("Ambos nombres difieren en las iniciales")
+elif nombre[-1]==nombre1[-1]:
+    print("Ambos nombres terminan con la misma letra")
+elif nombre[-1]!=nombre1[-1]:
+    print("Ambos nombres difieren en la ultima letra")
--- Unidad-2-python/Guias/Guia-1/ejercicio5.py
+++ Unidad-2-python/Guias/Guia-1/ejercicio5.py
@@ -1,5 +1,5 @@
 '''
-Crear un algoritmo que solicite por consola las 3 notas de los laboratorios realizados en el ramo de Programaci´on. Luego obtener el promedio ponderado de la
+Crear un algoritmo que solicite por consola las 3 notas de los laboratorios realizados en el ramo de Programacion. Luego obtener el promedio ponderado de la
 siguiente manera:
 Promedio Ponderado = Lab1 * 0,3 + Lab2 * 0,4 + Lab3 * 0,3
 Si el promedio es menor a 4,0 debe imprimir el mensaje que el estudiante reprobo
@@ -8,3 +8,14 @@ la asignatura. Si la nota es superior 6,0, debe mostrar el mensaje: el estudiant
 aprobo con distincion.
 
 '''
+lab1=float(input("Ingrese la nota del laboratorio 1 \n"))
+lab2=float(input("Ingrese la nota del laboratorio 2 \n"))
+lab3=float(input("Ingrese la nota del laboratorio 3 \n"))
+
+Promedio_ponderado= lab1 * 0.3 + lab2 * 0.4 + lab3 * 0.3
+
+if Promedio_ponderado >= 4.0 and Promedio_ponderado < 6.0:
+    print("El estudiante aprobo")
+elif Promedio_ponderado < 4.0:
+    print("El estudiante reprobo")
+else: print("El estudiante aprobo con distincion")
