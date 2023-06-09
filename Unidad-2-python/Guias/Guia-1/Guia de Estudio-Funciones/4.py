77dc5fd537b35c8774199b145de98bd32bfbca86
dolchKT4 <120228067+notKT4@users.noreply.github.com>
2023-06-09 10:21:14 +0100

    09/06

--- Unidad-2-python/Guias/Guia-1/Guia de Estudio-Funciones/4.py
+++ Unidad-2-python/Guias/Guia-1/Guia de Estudio-Funciones/4.py
@@ -1,5 +1,5 @@
 '''
-Disenar un algoritmo que pueda actuar como un cajero, que devuelve y desglosa el vuelto ˜
+Disenar un algoritmo que pueda actuar como un cajero, que devuelve y desglosa el vuelto
 en billetes y monedas (pesos chilenos). Utilizando funciones en Python.
 
 '''
